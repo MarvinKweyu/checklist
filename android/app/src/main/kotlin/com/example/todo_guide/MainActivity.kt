66b2521e15c417d863f1dc104a57a0b67277c98c
MarvinKweyu <mkweyu1@gmail.com>
2024-08-01 16:16:31 +0300

    feat: include database deps
    refac: update projeect name

--- android/app/src/main/kotlin/com/example/todo_guide/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/todo_guide/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.todo_guide
+package com.example.checklist
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/todo_bloc/todo_bloc.dart
+++ lib/todo_bloc/todo_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:todo_guide/data/todo.dart';
+import 'package:checklist/data/todo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
