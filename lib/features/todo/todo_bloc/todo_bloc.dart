353a154ded6c65e0b88b92a68443fd6235760e70
MarvinKweyu <mkweyu1@gmail.com>
2024-08-01 16:20:17 +0300

    refac: feature struct

--- lib/features/todo/todo_bloc/todo_bloc.dart
+++ lib/features/todo/todo_bloc/todo_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:checklist/data/todo.dart';
+import 'package:checklist/features/todo/data/todo.dart';
 
 part 'todo_event.dart';
 part 'todo_state.dart';
